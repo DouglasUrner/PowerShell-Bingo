c9e0caabfa0e84aaceb6d4f9d60b3480ec0448f2
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2023-03-21 23:31:25 -0700

    Randomize file creation, start task list.

---
+++ Setup/Tasks.md
@@ -0,0 +1,27 @@
+Display list of verbs
+Find powershell command with a name that matches a pattern
+Get help on a command
+Find out the properties & methods for an object
+
+Display the date/time a file was created
+Display the date/time a file was modified
+Display the date/time a file was last accessed
+Display all of a file's timestampss
+Display file length
+Sort output
+Display file owner
+Display directory contents
+Display file or directory permissions
+Find the largest file in a directory
+Find the largest file in a directory tree
+Make a list of the oldest files in a directory
+Calculate the size of a directory
+Count the number of files in a directory
+Find files that were changed today (or this week, or this year, or ...)
+Find files that were created today
+Find files that were accessed today
+Find files that have not been accessed in a year
+Select information from output objects
+Find all of the files of a particular type in a directory
+Count the number of directories in a directory tree
+Find files that match a particular name (e.g. Unity-Lock)
--- Setup/setup.ps1
+++ Setup/setup.ps1
@@ -14,13 +14,43 @@ function New-BingoFile {
     $f.CreationTime = "1953-10-30"
 }
 
-#New-BingoFile C:/users/urner/Desktop/GitHub/PowerShell-Bingo/Setup/foo 2MB
-
-$Path = "C:/users/urner/Desktop/GitHub/PowerShell-Bingo/Setup/tmp/"
-
-for ($i = 0; $i -lt 1000; $i++) {
-    $FullPath = $Path + $i + ".mt"
-    <# Action that will repeat until the condition is met #>
-    New-BingoFile $FullPath 1KB
+if ($IsMacOS)
+{
+    $Path = "$HOME/Source/GitHub/"
+} elseif ($IsWindows) {
+    $Path = "C:/users/urner/Desktop/GitHub/"
 }
+$Path = $Path + "PowerShell-Bingo/Setup/tmp/"
+
+if (!(test-path -PathType container $Path))
+{
+      New-Item -ItemType Directory -Path $Path
+}
+
+$DirMin  = 8
+$Dir1Max = 64
+$Dir2Max = 32
+$FileMax = 512
+
+for ($i = 0; $i -lt (Get-Random -Minimum $DirMin -Maximum $Dir1Max); $i++) {
+    $Dir1 = $Path + "/" + $i
+    if (!(test-path -PathType container $Dir1))
+    {
+        New-Item -ItemType Directory -Path $Dir1
+    }
+    for ($j = 0; $j -lt (Get-Random -Minimum $DirMin -Maximum $Dir2Max); $j++) {
+        $Dir2 = $Dir1 + "/" + $j
+        if (!(test-path -PathType container $Dir2))
+        {
+            New-Item -ItemType Directory -Path $Dir2
+        }
+        for ($k = 0; $k -lt (Get-Random -Maximum $FileMax); $k++) {
+            $FullPath = $Dir2 + "/" + $k + ".mt"
+            New-BingoFile $FullPath (Get-Random -Maximum 32768)
+        }
+    }
+}
+
+Write-Host ( Get-ChildItem -recurse $Path | Measure-Object ).Count
+
 #Read-Host -Prompt "Press Enter to exit"
